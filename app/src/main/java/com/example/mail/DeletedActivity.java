ea5cf64e506cf307c1908ef66445d29a09136637
LHappiness <2987366286@qq.com>
2024-11-14 23:59:15 +0800

    v1

---
+++ app/src/main/java/com/example/mail/DeletedActivity.java
@@ -0,0 +1,40 @@
+package com.example.mail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class DeletedActivity extends AppCompatActivity {
+//复制的detail的
+    private EditText edfrom;
+    private EditText edto;
+    private EditText eddate;
+    private EditText edsubject;
+    private EditText edcontent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+//        edfrom = findViewById(R.id.afrom);
+//        edto = findViewById(R.id.ato);
+//        eddate = findViewById(R.id.adate);
+//        edsubject = findViewById(R.id.asubject);
+//        edcontent = findViewById(R.id.acontent);
+//
+//        Intent intent = getIntent();
+//        int openMode = intent.getIntExtra("mode",1);
+//        if(openMode==3) {
+//
+//            edfrom.setText(intent.getStringExtra("from"));
+//            edto.setText(intent.getStringExtra("to"));
+//            eddate.setText(intent.getStringExtra("date"));
+//            edsubject.setText(intent.getStringExtra("subject"));
+//            edcontent.setText(intent.getStringExtra("content"));
+//        }
+    }
+}
--- app/src/main/java/com/example/mail/HomeActivity.java
+++ app/src/main/java/com/example/mail/HomeActivity.java
@@ -10,6 +10,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,10 +36,13 @@ import okhttp3.Response;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnReceiver;
-    private Button btnSender;
-    private FloatingActionButton floatingActionButton;
-    private TextView tvNickname;
+    private TextView userEmail;
+    private LinearLayout writeEmailLayout;
+    private LinearLayout inboxLayout;
+    private LinearLayout sentLayout;
+    private LinearLayout deletedLayout;
+    private LinearLayout logoutLayout;
+
     private String ip;
     Handler handler;
 
@@ -47,42 +51,60 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnReceiver = findViewById(R.id.ReceiverBox);
-        btnSender = findViewById(R.id.SenderBox);
-        floatingActionButton = findViewById(R.id.Add);
-        tvNickname = findViewById(R.id.NickName);
+        userEmail = findViewById(R.id.userEmail);
+        writeEmailLayout = findViewById(R.id.writeEmailLayout);
+        inboxLayout = findViewById(R.id.inboxLayout);
+        sentLayout = findViewById(R.id.sentLayout);
+        deletedLayout = findViewById(R.id.deletedLayout);
+        logoutLayout = findViewById(R.id.logoutLayout);
+
 
         //取出变量
         MyApplication application = (MyApplication) this.getApplicationContext();
         ip = application.getNumber();
 
-        handler = new Handler(Looper.getMainLooper()) {
-            @SuppressLint("HandlerLeak")
-            @Override
-            public void handleMessage(Message msg) {
-                if (msg.what == 1) {
-                    // 动态更新数据UI界面
-                    String str = msg.getData().getString("res") + "";//获取值时相应的类型要对应，传入为String类型用getString；Int类型用getInt。
-                    try {
-                        JSONObject jsonObject1 = new JSONObject(str);
-                        int code = jsonObject1.getInt("code");
-                        Gson gson=new Gson();
-                        User user = gson.fromJson(jsonObject1.getString("data"), new TypeToken<User>(){}.getType());
-                        System.out.println(user);
-                        tvNickname.setText(user.getNickName());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-
+//        handler = new Handler(Looper.getMainLooper()) {
+//            @SuppressLint("HandlerLeak")
+//            @Override
+//            public void handleMessage(Message msg) {
+//                if (msg.what == 1) {
+//                    // 动态更新数据UI界面
+//                    String str = msg.getData().getString("res") + "";//获取值时相应的类型要对应，传入为String类型用getString；Int类型用getInt。
+//                    try {
+//                        JSONObject jsonObject1 = new JSONObject(str);
+//                        int code = jsonObject1.getInt("code");
+//                        Gson gson=new Gson();
+//                        User user = gson.fromJson(jsonObject1.getString("data"), new TypeToken<User>(){}.getType());
+//                        System.out.println(user);
+//                        tvNickname.setText(user.getNickName());
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        };
 //        getNickName();
         SharedPreferencesUtil util = SharedPreferencesUtil.getInstance(HomeActivity.this);
-        tvNickname.setText(util.readString("username"));
+        userEmail.setText(util.readString("username"));
 
         //实现跳转
-        btnReceiver.setOnClickListener(new View.OnClickListener() {
+        userEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, InfoActivoty.class);
+                startActivity(intent);
+            }
+        });
+        //实现跳转
+        writeEmailLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+        //实现跳转
+        inboxLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, ReceiverActivity.class);
@@ -90,17 +112,9 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        tvNickname.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, InfoActivoty.class);
-                startActivity(intent);
-            }
-        });
-
 
         //实现跳转
-        btnSender.setOnClickListener(new View.OnClickListener() {
+        sentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, SenderActivity.class);
@@ -109,70 +123,78 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         //实现跳转
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        deletedLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, AddActivity.class);
+                Intent intent = new Intent(HomeActivity.this, DeletedActivity.class);
                 startActivity(intent);
             }
         });
-    }
-
-    public void getNickName() {
-        String ok = "获取成功";
-        String err = "网络错误";
-        String empty = "空错误";
-        SharedPreferencesUtil util = SharedPreferencesUtil.getInstance(HomeActivity.this);
-        String account = util.readString("user");
-        new Thread(new Runnable() {
+        //实现跳转
+        logoutLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                MediaType JSON = MediaType.parse("application/json;charset=utf-8");
-                JSONObject jsonObject = new JSONObject();
-                OkHttpClient httpClient = new OkHttpClient();
-                try {
-                    jsonObject.put("account", account);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                RequestBody requestBody = RequestBody.create(JSON, String.valueOf(jsonObject));
-                String url = "http://"+ip+":8080/people/queryPeopleMsg";
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(requestBody)
-                        .build();
-
-                Call call = httpClient.newCall(request);
-                call.enqueue(new Callback() {
-
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        Looper.prepare();
-                        Toast.makeText(getApplicationContext(), err, Toast.LENGTH_SHORT).show();
-                        Looper.loop();
-                    }
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        String MyResult = response.body().string();
-                        try {
-                            JSONObject jsonObject1 = new JSONObject(MyResult);
-                            int code = jsonObject1.getInt("state");
-                            System.out.println(code);
-                            Message msg = new Message();//创建信使（很形象的理解）
-                            msg.what = 1;//给信使做标记
-                            Bundle bundle = new Bundle();//创建放数据的容器
-
-                            bundle.putString("res",MyResult);
-                            msg.setData(bundle);
-                            handler.sendMessage(msg);	// handler传递参数
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            public void onClick(View v) {
+             //   Intent intent = new Intent(HomeActivity.this, AddActivity.class);
+             //   startActivity(intent);
             }
-        }).start();
+        });
     }
+//
+//    public void getNickName() {
+//        String ok = "获取成功";
+//        String err = "网络错误";
+//        String empty = "空错误";
+//        SharedPreferencesUtil util = SharedPreferencesUtil.getInstance(HomeActivity.this);
+//        String account = util.readString("user");
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                MediaType JSON = MediaType.parse("application/json;charset=utf-8");
+//                JSONObject jsonObject = new JSONObject();
+//                OkHttpClient httpClient = new OkHttpClient();
+//                try {
+//                    jsonObject.put("account", account);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//                RequestBody requestBody = RequestBody.create(JSON, String.valueOf(jsonObject));
+//                String url = "http://"+ip+":8080/people/queryPeopleMsg";
+//                Request request = new Request.Builder()
+//                        .url(url)
+//                        .post(requestBody)
+//                        .build();
+//
+//                Call call = httpClient.newCall(request);
+//                call.enqueue(new Callback() {
+//
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//                        Looper.prepare();
+//                        Toast.makeText(getApplicationContext(), err, Toast.LENGTH_SHORT).show();
+//                        Looper.loop();
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//                        String MyResult = response.body().string();
+//                        try {
+//                            JSONObject jsonObject1 = new JSONObject(MyResult);
+//                            int code = jsonObject1.getInt("state");
+//                            System.out.println(code);
+//                            Message msg = new Message();//创建信使（很形象的理解）
+//                            msg.what = 1;//给信使做标记
+//                            Bundle bundle = new Bundle();//创建放数据的容器
+//
+//                            bundle.putString("res",MyResult);
+//                            msg.setData(bundle);
+//                            handler.sendMessage(msg);	// handler传递参数
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                });
+//            }
+//        }).start();
+//    }
 
 }
